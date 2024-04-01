e0bdb773844b7beff7581d002dd99f520f8f5f15
ILHAMPARHADI <146052126+ILHAMPARHADI@users.noreply.github.com>
2024-04-01 12:05:33 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,20 @@
+# Belajar Variabel
+# assistgame
+x = 3
+y = 6
+uang = 10000
+
+#pemanggilan
+print("Nilai UAS Saya Adalah =", x)
+print("Nilai UTS Saya Adalah =", y)
+print("Nilai Sisa UANG Saya Adalah =", uang)
+
+#penamaan variabel
+nilai_a = 6 # tidak boleh spasi
+kotak10 = 10 # tidak boleh di awali angka
+
+#Imput variabel kedua
+x = 64
+print("Nilai UAS Saya Adalah =", x)
+x = 70
+print("Nilai UAS Saya Adalah =", x)
