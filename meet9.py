0c660a0b4f2058a389058fa24835ed5f58ec86c2
ILHAMPARHADI <146052126+ILHAMPARHADI@users.noreply.github.com>
2024-06-03 11:50:05 +0800

    Add files via upload

---
+++ meet9.py
@@ -0,0 +1,23 @@
+# percabangan jika maka
+
+name = input("what is your name : ")
+
+#if komdisi : aksi
+
+#if standar
+#if name == "ilham" : print("Mahasisswa ganteng")
+#print("end of program")
+
+# 2. if identasi
+#if name=="ilham":
+#    print("Mahasiswa ganteng")
+#    print("ganteng banget")
+#print("end of program")
+
+# 3. IF ELSE
+if name=="ilham":
+    print("mahasiswa ganteng")
+    print(f"terimakasih {name}")
+else:
+    print("anda tidak ganteng")
+print("end of program")
